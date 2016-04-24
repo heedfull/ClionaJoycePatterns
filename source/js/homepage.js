2d78d92e8d93314f7f6af3671ddd34cdea8bea98
Edwin Goddard <edwin.goddard@gmail.com>
2016-04-24 22:20:01 +0100

    adding flickity

---
+++ source/js/homepage.js
@@ -0,0 +1,8 @@
+var elem = document.querySelector('.main-gallery');
+var flkty = new Flickity( elem, {
+  // options
+  // cellAlign: 'left',
+  // contain: true
+  setGallerySize: false,
+  wrapAround: true
+});
